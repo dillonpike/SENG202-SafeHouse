ec2e163519edd81b655a890f23c896f150534f97
sne69 <sne69@uclive.ac.nz>
2021-08-09 14:35:30 +1200

    Adding JUnit Test File

---
+++ src/test/java/seng202/team8/SahilTest.java
@@ -0,0 +1,18 @@
+package seng202.team8;
+
+import junit.framework.TestCase;
+
+/**
+ * A simple JUnit test
+ */
+public class SahilTest extends TestCase {
+
+    /**
+     * Basic Test
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+
+}
