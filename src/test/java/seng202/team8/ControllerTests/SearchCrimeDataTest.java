0fa0d9ee619b2c21ae2cd0792781ed63aa0b1d85
sne69 <sne69@uclive.ac.nz>
2021-08-21 00:40:30 +1200

    Implemented SearchCrimeData class, where it contains all the methods to filter the data based on the user's input

---
+++ src/test/java/seng202/team8/ControllerTests/SearchCrimeDataTest.java
@@ -0,0 +1,40 @@
+package seng202.team8.ControllerTests;
+
+import junit.framework.TestCase;
+import seng202.team8.Model.CrimeRecord;
+
+import java.util.ArrayList;
+
+public class SearchCrimeDataTest extends TestCase {
+
+    public void testGetCrimeRecordData() {
+        /**
+         *
+         */
+
+//        ArrayList<CrimeRecord> crimeRecordData = new ArrayList<CrimeRecord>();
+//        crimeRecordData.add(new CrimeRecord());
+
+    }
+
+    public void testSetCrimeRecordData() {
+    }
+
+    public void testFilterByDate() {
+    }
+
+    public void testFilterByCrimeType() {
+    }
+
+    public void testFilterByCrimeLocation() {
+    }
+
+    public void testFilterByCrimeLocationBeat() {
+    }
+
+    public void testFilterByArrest() {
+    }
+
+    public void testFilterByDomesticViolence() {
+    }
+}
