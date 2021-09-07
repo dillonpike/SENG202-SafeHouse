cd590bd81d3069304b290cd397c1e36edbaf8c18
sne69 <sne69@uclive.ac.nz>
2021-09-07 20:08:02 +1200

    Removed the get and set method for Arraylist of Crime record. And added a parameter of Arraylist of crime record data to all the methods

--- src/main/java/seng202/team8/controller/SearchCrimeData.java
+++ src/main/java/seng202/team8/controller/SearchCrimeData.java
@@ -11,40 +11,7 @@ import seng202.team8.model.CrimeRecord;
  * A class that contains methods to filter the crime data list based on the command given
  */
 public class SearchCrimeData {
-
-    /**
-     * List of crime records.
-     */
-    private static ArrayList<CrimeRecord> crimeRecordData;
-
-    public SearchCrimeData() {}
-
-    /**
-     * A constructor, that sets the ArrayList of the given CrimeRecord list.
-     * @param crimeRecordData, the param gets the crime record data list
-     */
-    public SearchCrimeData(ArrayList<CrimeRecord> crimeRecordData) {
-        SearchCrimeData.crimeRecordData = crimeRecordData;
-    }
-
-
-    /**
-     * Returns the crime record data list
-     * @return crimeRecordData, it returns the crime record data list
-     */
-    public static ArrayList<CrimeRecord> getCrimeRecordData() {
-        return crimeRecordData;
-    }
-
-    /**
-     * Sets the given crime data list to the new crime record list
-     * @param crimeRecordData, the param gets crime record data list
-     */
-    public void setCrimeRecordData(ArrayList<CrimeRecord> crimeRecordData) {
-        SearchCrimeData.crimeRecordData = crimeRecordData;
-    }
-
-
+    
     /**
      * Filters the crime record data list by checking that each crime date is within the
      * given two date. If the date is within the dates, then it adds to the new list, and at the
@@ -54,7 +21,8 @@ public class SearchCrimeData {
      * @return filterByDateList, a new list made based on the dates between start and end date included
      * @throws ParseException If fail to parse string that is going to be saved as a specific format.
      */
-    public static ArrayList<CrimeRecord> filterByDate(String startDate, String endDate) throws ParseException {
+    public static ArrayList<CrimeRecord> filterByDate(ArrayList<CrimeRecord> crimeRecordData, String startDate,
+                                                      String endDate) throws ParseException {
         ArrayList<CrimeRecord> filterByDateList = new ArrayList<>();
 
         for (CrimeRecord crimeData : crimeRecordData) {
@@ -82,7 +50,7 @@ public class SearchCrimeData {
      * @param crimeDescription, gets the crime description of a location
      * @return filterByCrimeTypeList, the new list based on the crime description type
      */
-    public static ArrayList<CrimeRecord> filterByCrimeType(String crimeDescription) {
+    public static ArrayList<CrimeRecord> filterByCrimeType(ArrayList<CrimeRecord> crimeRecordData, String crimeDescription) {
         ArrayList<CrimeRecord> filterByCrimeTypeList = new ArrayList<>();
 
         for (CrimeRecord crimeData : crimeRecordData) {
@@ -101,7 +69,7 @@ public class SearchCrimeData {
      * @param locationName, gets the name of the crime location
      * @return filterByCrimeLocationList, the new list based on the crime location
      */
-    public static ArrayList<CrimeRecord> filterByCrimeLocation(String locationName) {
+    public static ArrayList<CrimeRecord> filterByCrimeLocation(ArrayList<CrimeRecord> crimeRecordData, String locationName) {
         ArrayList<CrimeRecord> filterByCrimeLocationList = new ArrayList<>();
 
         for (CrimeRecord crimeData : crimeRecordData) {
@@ -122,7 +90,8 @@ public class SearchCrimeData {
      * @return filterByCrimeLocationBeat, the new list based on the beat numbers between start
      * and end beat numbers included
      */
-    public static ArrayList<CrimeRecord> filterByCrimeLocationBeat(int startBeatNum, int endBeatNum) {
+    public static ArrayList<CrimeRecord> filterByCrimeLocationBeat(ArrayList<CrimeRecord> crimeRecordData,
+                                                                   int startBeatNum, int endBeatNum) {
         ArrayList<CrimeRecord> filterByCrimeLocationBeat = new ArrayList<>();
 
         for (CrimeRecord crimeData : crimeRecordData) {
@@ -142,7 +111,8 @@ public class SearchCrimeData {
      * @return filterByCrimeWard, the new list based on the ward numbers between start
      * and end ward numbers included
      */
-    public static ArrayList<CrimeRecord> filterByCrimeWard(int startWardNum, int endWardNum) {
+    public static ArrayList<CrimeRecord> filterByCrimeWard(ArrayList<CrimeRecord> crimeRecordData,
+                                                           int startWardNum, int endWardNum) {
         ArrayList<CrimeRecord> filterByCrimeWard = new ArrayList<>();
 
         for (CrimeRecord crimeData : crimeRecordData) {
@@ -161,7 +131,7 @@ public class SearchCrimeData {
      * @param arrestMade, gets the boolean as true for 'Y' (Yes) and false for 'N' (No)
      * @return filterByArrestList, a new list based on the arrest has been made or not
      */
-    public static ArrayList<CrimeRecord> filterByArrest(boolean arrestMade) {
+    public static ArrayList<CrimeRecord> filterByArrest(ArrayList<CrimeRecord> crimeRecordData, boolean arrestMade) {
         ArrayList<CrimeRecord> filterByArrestList = new ArrayList<>();
 
         for (CrimeRecord crimeData : crimeRecordData) {
@@ -181,7 +151,7 @@ public class SearchCrimeData {
      * @return filterByDomesticViolenceList, a new list based on whether there was domestic
      * violence or not
      */
-    public static ArrayList<CrimeRecord> filterByDomesticViolence(boolean wasDomesticViolence) {
+    public static ArrayList<CrimeRecord> filterByDomesticViolence(ArrayList<CrimeRecord> crimeRecordData, boolean wasDomesticViolence) {
         ArrayList<CrimeRecord> filterByDomesticViolenceList = new ArrayList<>();
 
         for (CrimeRecord crimeData : crimeRecordData)
@@ -199,7 +169,7 @@ public class SearchCrimeData {
      * sets and returns it.
      * @return rankedKeySet, a set containing ranked data based on the ward areas
      */
-    public static Set rankByMostDangerousAreas() {
+    public static Set rankByMostDangerousAreas(ArrayList<CrimeRecord> crimeRecordData) {
         HashMap<Integer, Integer> rankByMostDangerousAreasMap = new HashMap<>();
 
         for (CrimeRecord crimeData : crimeRecordData) {
@@ -235,7 +205,7 @@ public class SearchCrimeData {
      * @return rankedKeySet, a set containing ranked data based on the crime primary
      * description type
      */
-    public static Set rankByCrimeTypeFrequency() {
+    public static Set rankByCrimeTypeFrequency(ArrayList<CrimeRecord> crimeRecordData) {
         HashMap<String, Integer> rankByCrimeTypeFrequencyMap = new HashMap<>();
 
         for (CrimeRecord crimeData : crimeRecordData) {
@@ -273,7 +243,7 @@ public class SearchCrimeData {
      * @return highAndLowCrimeRatesList, a list[] containing high crime rates ward list at
      * index 0, and low crime rates ward list at index 1
      */
-    public static List[] wardByHighAndLowCrimeRates() {
+    public static List[] wardByHighAndLowCrimeRates(ArrayList<CrimeRecord> crimeRecordData) {
         HashMap<Integer, Integer> rankByMostDangerousAreasMap = new HashMap<>();
 
         for (CrimeRecord crimeData : crimeRecordData) {
@@ -303,26 +273,6 @@ public class SearchCrimeData {
 
         return highAndLowCrimeRatesList;
     }
-
-    public static void main(String[] args) {
-        ArrayList<CrimeRecord> crimeData = new ArrayList<CrimeRecord>();
-        crimeData.add(new CrimeRecord("JE163990", 11, 23, 2020, "03:05:00 PM",
-                "073XX S SOUTH SHORE DR", "4386", "THEFT", "$500 AND UNDER",
-                "APARTMENT", 0, 0, 334, 7, "6", 41.748486365, -87.602675062));
-        crimeData.add(new CrimeRecord("JE266959",6, 15, 2021, "01:30:00 PM", "018XX N DAMEN AVE", "0460",
-                "BATTERY", "SIMPLE", "PARK PROPERTY", 0, 0, 1434, 32,"08B",41.914562993,-87.677553434));
-        crimeData.add(new CrimeRecord("JE266568",6, 15, 2021, "01:30:00 AM", "055XX N MC VICKER AVE", "0810", "THEFT", "OVER $500",
-                "STREET",0,0,1622,45,"06", 41.982167364,-87.779228689));
-        crimeData.add(new CrimeRecord("JE267494", 6, 15, 2021, "11:04:00 PM","052XX S ARTESIAN AVE","0110","HOMICIDE",
-                "FIRST DEGREE MURDER","STREET",0,0,923,7,"01A",41.798854705,-87.685390881));
-        crimeData.add(new CrimeRecord("JE267206",6, 15, 2021, "05:20:00 PM","048XX N PULASKI RD","0460","BATTERY",
-                "SIMPLE","PARKING LOT / GARAGE (NON RESIDENTIAL)",0,0,1712,32,"08B",41.969090767,-87.728052386));
-
-        SearchCrimeData sr = new SearchCrimeData(crimeData);
-        Set data = sr.rankByMostDangerousAreas();
-        System.out.println(data);
-    }
-
 
 }
 
--- src/test/java/seng202/team8/controller/SearchCrimeDataTest.java
+++ src/test/java/seng202/team8/controller/SearchCrimeDataTest.java
@@ -1,7 +1,9 @@
 package seng202.team8.controller;
 
 import junit.framework.TestCase;
-import seng202.team8.controller.SearchCrimeData;
+import seng202.team8.model.CrimeRecord;
+
+import java.util.ArrayList;
 
 /**
  * A test class that checks SearchCrimeData class methods, whether their input and output are
@@ -58,5 +60,31 @@ public class SearchCrimeDataTest extends TestCase {
     }
 
     public void testFilterByDomesticViolence() {
+    }
+
+    /**
+     * For testing the method from SearchCrimeData class
+     * @return crimeRecordData, a crime record data list of CrimeRecord
+     */
+    public ArrayList<CrimeRecord> getCrimeDataForTesting() {
+        ArrayList<CrimeRecord> crimeRecordData = new ArrayList<>();
+        crimeRecordData.add(new CrimeRecord("JE266628", 6, 15, 2021, "09:30:00 AM",
+                "080XX S DREXEL AVE", "0820", "THEFT", "$500 AND UNDER", "STREET", 0, 0,
+                631, 8, "06", 41.748486365, -87.602675062));
+        crimeRecordData.add(new CrimeRecord("JE266473",6, 15, 2021, "07:47:00 AM",
+                "062XX S MORGAN ST","0110","HOMICIDE","FIRST DEGREE MURDER","APARTMENT",0,0,
+                712,16,"01A",41.780850996,-87.649674221));
+        crimeRecordData.add(new CrimeRecord("JE163990", 11, 23, 2020, "03:05:00 PM",
+                "073XX S SOUTH SHORE DR", "4386", "THEFT", "$500 AND UNDER",
+                "APARTMENT", 0, 0, 334, 7, "6", 41.748486365, -87.602675062));
+        crimeRecordData.add(new CrimeRecord("JE266959",6, 15, 2021, "01:30:00 PM", "018XX N DAMEN AVE", "0460",
+                "BATTERY", "SIMPLE", "PARK PROPERTY", 0, 0, 1434, 32,"08B",41.914562993,-87.677553434));
+        crimeRecordData.add(new CrimeRecord("JE266568",5, 15, 2021, "02:45:00 AM", "055XX N MC VICKER AVE", "0810", "THEFT", "OVER $500",
+                "STREET",0,0,1622,45,"06", 41.867038835,-87.656992959));
+        crimeRecordData.add(new CrimeRecord("JE267494", 6, 15, 2021, "11:04:00 PM","052XX S ARTESIAN AVE","0110","HOMICIDE",
+                "FIRST DEGREE MURDER","STREET",0,0,923,7,"01A",41.798854705,-87.685390881));
+        crimeRecordData.add(new CrimeRecord("JE267206",6, 15, 2021, "05:20:00 PM","048XX N PULASKI RD","0460","BATTERY",
+                "SIMPLE","PARKING LOT / GARAGE (NON RESIDENTIAL)",0,0,1712,32,"08B",41.969090767,-87.728052386));
+        return crimeRecordData;
     }
 }
