f9f768eb75c66aecc5bab2ccb4dc614be8ce74cd
Karl Moore <krm97@uclive.ac.nz>
2021-09-21 12:13:26 +1200

    Added UI warnings for invalid data in new record creation

--- src/main/java/seng202/team8/controller/AddRecordController.java
+++ src/main/java/seng202/team8/controller/AddRecordController.java
@@ -8,6 +8,12 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 import seng202.team8.model.CrimeRecord;
 import javafx.fxml.Initializable;
 
@@ -65,11 +71,93 @@ public class AddRecordController extends Controller implements Initializable {
     private Button btnClose;
 	
     public void createRecord() {
-    	CrimeRecord newRecord = new CrimeRecord(fldCaseNum.getText(), fldDate.getValue().getMonthValue(), fldDate.getValue().getDayOfMonth(),
-    			fldDate.getValue().getYear(), "0:00", fldBlock.getText(), fldIUCR.getText(), fldPrimaryDesc.getText(), fldSecondaryDesc.getText(),
-    			"tempLoc", -1, -1, (int)Integer.valueOf(fldBeat.getText()), 
-    			(int)Integer.valueOf(fldWard.getText()), fldFBI.getText(), (double)Double.valueOf(fldLat.getText()), (double)Double.valueOf(fldLon.getText()));
-    	System.out.println(newRecord.toString());
+    	if (checkCaseNum() && checkFBI() && checkIUCR() && checkPrimary() && checkWard() && checkBeat() && checkLat() && checkLon() == true) {
+    		CrimeRecord newRecord = new CrimeRecord(fldCaseNum.getText(), fldDate.getValue().getMonthValue(), fldDate.getValue().getDayOfMonth(),
+    		fldDate.getValue().getYear(), "0:00", fldBlock.getText(), fldIUCR.getText(), fldPrimaryDesc.getText(), fldSecondaryDesc.getText(),
+    		"tempLoc", -1, -1, (int)Integer.valueOf(fldBeat.getText()), 
+    		(int)Integer.valueOf(fldWard.getText()), fldFBI.getText(), (double)Double.valueOf(fldLat.getText()), (double)Double.valueOf(fldLon.getText()));
+    		System.out.println(newRecord.toString());
+    	}
+    }
+    
+    public boolean checkCaseNum() {
+    	if (ValidateCrime.validateCaseNum(fldCaseNum.getText())== false) {
+    		fldCaseNum.setStyle("-fx-background-color: red, white; -fx-background-insets: 0, 1; -fx-background-radius: 1px, 0px");
+    		return false;
+    	} else {
+    		fldCaseNum.setStyle(null);
+    		return true;
+    	}
+    }
+    
+    public boolean checkFBI() {
+    	if (ValidateCrime.validateFbiCD(fldFBI.getText()) == false) {
+    		fldFBI.setStyle("-fx-background-color: red, white; -fx-background-insets: 0, 1; -fx-background-radius: 1px, 0px");
+    		return false;
+    	} else {
+    		fldFBI.setStyle(null);
+    		return true;
+    	}
+    }
+    
+    public boolean checkIUCR() {
+    	if (ValidateCrime.validateIucr(fldIUCR.getText()) == false) {
+    		fldIUCR.setStyle("-fx-background-color: red, white; -fx-background-insets: 0, 1; -fx-background-radius: 1px, 0px");
+    		return false;
+    	} else {
+    		fldIUCR.setStyle(null);
+    		return true;
+    	}
+    }
+    	
+    public boolean checkPrimary() {
+    	if (ValidateCrime.validatePrimary(fldPrimaryDesc.getText()) == false) {
+    		fldPrimaryDesc.setStyle("-fx-background-color: red, white; -fx-background-insets: 0, 1; -fx-background-radius: 1px, 0px");
+    		return false;
+    	} else {
+    		fldPrimaryDesc.setStyle(null);
+    		return true;
+    	}
+    }
+    
+    public boolean checkWard() {
+    	if (ValidateCrime.validateInt(fldWard.getText()) == false) { 
+    		fldWard.setStyle("-fx-background-color: red, white; -fx-background-insets: 0, 1; -fx-background-radius: 1px, 0px");
+    		return false;
+    	} else {
+    		fldWard.setStyle(null);
+    		return true;
+    	}
+    }
+    	
+    public boolean checkBeat() {
+    	if (ValidateCrime.validateInt(fldBeat.getText()) == false) {
+    		fldBeat.setStyle("-fx-background-color: red, white; -fx-background-insets: 0, 1; -fx-background-radius: 1px, 0px");
+    		return false;
+    	} else {
+    		fldBeat.setStyle(null);
+    		return true;
+    	}
+    }
+    
+    public boolean checkLat() {
+    	if (ValidateCrime.validateDouble(fldLat.getText()) == false) {
+    		fldLat.setStyle("-fx-background-color: red, white; -fx-background-insets: 0, 1; -fx-background-radius: 1px, 0px");
+    		return false;
+    	} else {
+    		fldLat.setStyle(null);
+    		return true;
+    	}
+    }
+    
+    public boolean checkLon() {
+    	if (ValidateCrime.validateDouble(fldLon.getText()) == false) {
+    		fldLon.setStyle("-fx-background-color: red, white; -fx-background-insets: 0, 1; -fx-background-radius: 1px, 0px");
+    		return false;
+    	} else {
+    		fldLon.setStyle(null);
+    		return true;
+    	}
     }
     
 	@Override
