74f40c38e3600d55f3c6fdae4502e3269927d7ce
Oliver Johnson <ojo26@uclive.ac.nz>
2021-09-22 21:42:21 +1200

    Moved isInteger from TableController to Controller

--- src/main/java/seng202/team8/controller/Controller.java
+++ src/main/java/seng202/team8/controller/Controller.java
@@ -213,6 +213,21 @@ public class Controller {
     }
 
 	/**
+	 * Returns true if string can be converted to an integer, otherwise false.
+	 * @param string string that is checked
+	 * @return true if string can be converted to an integer, otherwise false
+	 */
+	public boolean isInteger(String string) {
+		try {
+			Integer.parseInt(string);
+			return true;
+		}
+		catch (NumberFormatException e) {
+			return false;
+		}
+	}
+
+	/**
 	 * Returns the controller's crime record manager.
 	 * @return crime record manager
 	 */
