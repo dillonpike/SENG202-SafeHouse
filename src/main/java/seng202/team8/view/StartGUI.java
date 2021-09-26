35bc2bab2c9e055ba01210a506b2b62fd33a18cc
Dillon Pike <dkp33@uclive.ac.nz>
2021-09-26 13:22:14 +1300

    Added empty readme file

--- src/main/java/seng202/team8/view/StartGUI.java
+++ src/main/java/seng202/team8/view/StartGUI.java
@@ -16,13 +16,6 @@ public class StartGUI extends Application {
 	public void start(Stage s) {
 		try {
 			primaryStage = s;
-			//primaryStage.initStyle(StageStyle.UNDECORATED);
-			//Parent root = FXMLLoader.load(getClass().getResource("frame.fxml"));
-			//Scene scene = new Scene(root,1280,720);
-			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			//primaryStage.setScene(scene);
-			//primaryStage.show();
-
 			control = new GUIController();
 			DataManager.addToDatasets(DataManager.getCurrentDataset());
 			control.start(primaryStage);
